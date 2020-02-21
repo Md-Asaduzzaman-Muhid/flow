a6419d9c5243d49ed3c4ab519f54dc08e4162c3d
Md-Asaduzzaman-Muhid <asaduzzaman.muhid@gmail.com>
2020-02-22 00:15:50 +0600

    table not exist create

--- common/dbconfig.php
+++ common/dbconfig.php
@@ -5,26 +5,29 @@
         private $USER_PASSWORD = '';
         private $DB_NAME = 'flow'; 
         public $conn;
-        public $sql = "CREATE TABLE IF NOT EXISTS user (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(30) NOT NULL,
-            email VARCHAR(30) NOT NULL,
-            phone VARCHAR(50),
-            dob VARCHAR(50),
-            image VARCHAR(50),
-            password VARCHAR(50),
-            phone VARCHAR(50),
-            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-            )";
+        
         public function dbConnection(){
+            $sql = "CREATE TABLE IF NOT EXISTS user1 (
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(30) NOT NULL,
+                email VARCHAR(30) NOT NULL,
+                phone VARCHAR(50),
+                dob VARCHAR(50),
+                image VARCHAR(50),
+                password VARCHAR(50),
+                phone VARCHAR(50),
+                reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP )";
         $this->conn = null;
             try{
                 $this->conn = mysqli_connect($this->DB_HOST, $this->USER_NAME, $this->USER_PASSWORD, $this->DB_NAME);
-                $this->conn->exec($sql);
+                if(mysqli_query($this->conn, $sql)){
+                    echo "Created";
+                }
                 
             }  catch (exception $ex){
                 echo "connection error".$ex;
             }
+            
             return $this->conn;
         }
     }
